6348b25653f6d150b1595fa21b85cd506932170a
Andrew Russell <67603030+ARussell-CF@users.noreply.github.com>
2021-01-21 12:55:55 -0500

    Initial commit

---
+++ Models/Palindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TacoCat.Models
+{
+    //This is a View Model - it helps my Views not my Database
+    public class Palindrome
+    {
+        //The word the user input
+        public string InputWord { get; set; }
+
+        //That word reversed
+        public string RevWord { get; set; }
+
+        //Is it a palindrome
+        public bool IsPalindrome { get; set; }
+
+        //What do I output?
+        public string Message { get; set; }
+
+    }
+}
